eac85a5d88a78abdce1e6f16a18121d6ead67579
HMOMOKI <momoki.hiromichi@d-plustrading.com>
2021-08-18 09:41:03 +0900

    navの作成

--- ui/src/router/index.js
+++ ui/src/router/index.js
@@ -1,20 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+  {path: '/',name: 'Home', component:() => import('../views/Home.vue')},
+  {path: '/login',name:'Login', component: () => import('../views/auth/Login.vue')},
+  {path: '/register',name:'Resister', component: () => import('../views/auth/Register.vue')},
 ]
 
 const router = createRouter({
